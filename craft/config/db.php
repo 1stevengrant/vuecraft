425e0e766f4c038176d44a2a882a84b2e8b1c91b
Steven Grant <steven@ghijk.co.uk>
2017-08-14 07:45:24 +0100

    These are the completed files for part 1 https://digitalevangelist.net/blog/vuejs-craft-cms-single-page-app-bootstrap-the-app

---
+++ craft/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'server' => getenv('DB_HOST'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'database' => getenv('DB_NAME'),
+
+);
---
+++ src/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ welcomeMessage }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      welcomeMessage: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../node_modules/bootstrap/scss/bootstrap"
+</style>
